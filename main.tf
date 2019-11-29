4d2efb03f94d16e61e1a9542ae53ea158d5a1397
alsamciklum <49643120+alsamciklum@users.noreply.github.com>
2019-11-29 21:58:29 +0200

    HomeTask3

--- main.tf
+++ main.tf
@@ -15,9 +15,39 @@ data "aws_ami" "centos" {
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.centos.id}"
   instance_type = "t2.micro"
+  key_name = "alsam"
   tags = {
     Name = "HelloWorld"
   }
-  security_groups = ["Alsam"]
-  key_name= "alsam"
-} 
+  security_groups = ["Hillel_alsam", "Alsam"]
+  provisioner "remote-exec" {
+    inline = [
+      "sudo -i",
+      "yum update",
+    ]
+    connection {
+      type = "ssh"
+      host = "${aws_instance.web.public_ip}"
+      user = "centos"
+      port = "22"
+      private_key = file("/home/alex/Documents/alsam-aws/alsam.pem")
+    }
+  }
+}
+
+resource "aws_security_group" "Hillel_alsam" {
+  name         = "Hillel_alsam"
+  description  = "Hillel security group"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["192.105.145.0/32"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
